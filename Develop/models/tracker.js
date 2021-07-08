fec8ed90a8b0b9f79d00e36a58d30568df0b05aa
Kthendavid04 <kthendavid04@gmail.com>
2021-07-07 21:49:35 -0500

    updated routes

---
+++ Develop/models/tracker.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const trackerSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Tracker = mongoose.model("Tracker", trackerSchema);
+
+module.exports = Transaction;
