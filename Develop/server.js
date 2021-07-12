1720dda041cffed057ac0c5254a63c2b82ab98f3
Kthendavid04 <kthendavid04@gmail.com>
2021-07-12 17:29:40 -0500

    deploying heroku

--- Develop/server.js
+++ Develop/server.js
@@ -13,11 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
-});
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 // routes
 app.use(require("./routes/api.js"));
