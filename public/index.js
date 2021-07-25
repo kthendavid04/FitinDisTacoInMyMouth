e95f9c9c15bf093f0bb77292b7fbdc27b68674ca
Kthendavid04 <kthendavid04@gmail.com>
2021-07-25 13:08:20 -0500

    updated index.html and index.js

--- public/index.js
+++ public/index.js
@@ -11,3 +11,9 @@ async function init() {
   }
 }
 
+document.getElementById("toast").onclick = function() {
+  var myAlert =document.getElementById('Workout added!');//select id of toast
+  var bsAlert = new bootstrap.Toast(myAlert);//inizialize it
+  bsAlert.show();//show it
+}
+
